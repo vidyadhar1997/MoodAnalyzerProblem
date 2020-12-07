861562b7b2ed2a9eda6318baa6668c402a18ac71
Vidyadhar1997 <vidyadharhudge1997@gmail.com>
2020-12-07 09:02:59 +0530

    [vidyadhar hudge]Add:Welcome Message

---
+++ MoodAnalyzerProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MoodAnalyzerProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To the Mood Analyzer Problem");
+        }
+    }
+}
