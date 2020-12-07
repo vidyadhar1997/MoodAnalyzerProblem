c263e48858840f39126d00db17c0af5e917bb9ae
Vidyadhar1997 <vidyadharhudge1997@gmail.com>
2020-12-07 16:19:36 +0530

    [vidyadhar hudge]Add:Ablity to analyse mood is happy or sad

---
+++ MoodAnalyzerProblem/MoodAnalyzer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodAnalyzerProblem
+{
+    class MoodAnalyzer
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To the Mood Analyzer Problem");
+        }
+        public string analyseMood(string message)
+        {
+            if (message.Equals("Happy"))
+            {
+                return "Happy Mood";
+            }
+            else 
+            {
+                return "Sad Mood";
+            }
+
+        }
+    }
+}
